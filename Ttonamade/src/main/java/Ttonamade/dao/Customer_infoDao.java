7e73b74382e690abb32067a7c3904dd3a0de05ad
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-09-01 15:13:55 +0900

    id에 One 없던 것들에 One 추가

--- Ttonamade/src/main/java/Ttonamade/dao/Customer_infoDao.java
+++ Ttonamade/src/main/java/Ttonamade/dao/Customer_infoDao.java
@@ -21,32 +21,7 @@ public class Customer_infoDao {
 		this.ss = ss;
 	}
 	
-<<<<<<< HEAD
-	public List<Customer_infoDto> selectAll() throws Exception{
-		
-		return ss.selectList(NameSpace + "selectAll");
-	}
-	
-	public Customer_infoDto selectOne(int cust_id) throws Exception{
-			
-			return ss.selectOne(NameSpace + "select", cust_id);
-		}
-	
-	public void insert(Customer_infoDto  custDto) throws Exception{
-		
-		ss.insert(NameSpace + "insert", custDto);
-		
-	}
-	public void delete(int cust_id) throws Exception{
-		
-		ss.delete(NameSpace + "delete", cust_id);
-		
-	}
-	
-	public void update(Customer_infoDto custDto) throws Exception{
-		
-		ss.update(NameSpace + "update", custDto);
-=======
+
 	public List<Customer_infoDto> selectAll(int cust_id) throws Exception{
 		
 		return ss.selectList(NameSpace + "selectAll" + cust_id);
@@ -70,9 +45,7 @@ public class Customer_infoDao {
 	
 	public void updateOne(Customer_infoDto custDto) throws Exception{
 		
-		ss.update(NameSpace + "updateOne", custDto);
->>>>>>> branch 'main' of https://github.com/DonggeonMoon/Ttonamade.git
-	
+		ss.update(NameSpace + "updateOne", custDto);	
 }
 
 
